aa617711d376b9b47f93766220ec396863212ace
Willian Briski <wyll_gustavo11@hotmail.com>
2024-09-02 14:28:03 -0300

    estilizando a sidebar

--- src/components/Paragrafo/styles.ts
+++ src/components/Paragrafo/styles.ts
@@ -4,7 +4,7 @@ import { Props } from '.'
 
 export const P = styled.p<Props>`
   color: #282a35;
-  font-size: 14px;
+  font-size: ${(props) => (props.fontSize ? props.fontSize + 'px' : '14px')};
   color: ${(props) => (props.tipo === 'principal' ? '#282a35' : '#949494')};
   line-height: 22px;
 `
