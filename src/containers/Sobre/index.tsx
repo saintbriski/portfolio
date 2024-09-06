7cb0e5176245d3e33df61599155302db2b91d050
Willian Briski <wyll_gustavo11@hotmail.com>
2024-09-06 09:38:30 -0300

    adicionando lógica para mudança de tema

--- src/containers/Sobre/index.tsx
+++ src/containers/Sobre/index.tsx
@@ -11,7 +11,7 @@ const Sobre = () => (
       facilis vero officiis beatae eveniet in delectus modi atque tenetur optio!
     </Paragrafo>
     <GithubSecao>
-      <img src="https://github-readme-stats.vercel.app/api?count_private=true%22&include_all_commits=true&show_icons=true&theme=dracula&username=saintbriski" />
+      <img src="https://github-readme-stats.vercel.app/api?username=saintbriski&show_icons=true&theme=dracula&include_all_commits=true&count_private=true" />
       <img src="https://github-readme-stats.vercel.app/api/top-langs/?username=saintbriski&layout=compact&langs_count=7&theme=dracula" />
     </GithubSecao>
   </section>
